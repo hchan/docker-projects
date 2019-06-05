88642e438437361788c40966e13c2bdfd6958539
Henry Chan <henry.chan@mec.ca>
2019-06-04 19:34:37 -0700

    uses xfce4

--- myUbuntu/docker-entrypoint.sh
+++ myUbuntu/docker-entrypoint.sh
@@ -2,5 +2,7 @@
 echo Hello World
 vnc4server > /tmp/vncserver.out 2>&1
 export DISPLAY=$(hostname):1
-gnome-terminal
+export XFCE_PANEL_MIGRATE_DEFAULT=true
+xfce4-session &
+xfce4-terminal &
 /usr/sbin/sshd -D
