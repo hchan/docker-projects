13a4ff916a81ea7c893de8e816dccd4db1245078
Henry Chan <henry.chan@mec.ca>
2019-06-04 18:09:25 -0700

    vnc is working

--- myUbuntu/docker-entrypoint.sh
+++ myUbuntu/docker-entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 echo Hello World
+vnc4server > /tmp/vncserver.out 2>&1
+export DISPLAY=$(hostname):1
+gnome-terminal
+/usr/sbin/sshd -D
--- myUbuntu/runMyUbuntu.sh
+++ myUbuntu/runMyUbuntu.sh
@@ -1,1 +1,1 @@
-docker run -d -p 22222:22 -v /:/data  my-ubuntu
+docker run -d -p 22222:22 -p 25901:5901 -v /:/data  my-ubuntu
