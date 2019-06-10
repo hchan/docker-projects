b276f0c7ed7306ff29ae610c865762dab850e242
hchan <hchan@apache.org>
2019-06-09 18:33:02 -0700

    xterm works with utf-8

--- myUbuntu/docker-entrypoint.sh
+++ myUbuntu/docker-entrypoint.sh
@@ -4,7 +4,9 @@ vnc4server -geometry 1600x800 > /tmp/vncserver.out 2>&1
 export DISPLAY=$(hostname):1
 export XFCE_PANEL_MIGRATE_DEFAULT=true
 xfce4-session &
+xhost +
 # sets the default to xfce4-terminal
 echo 2 | update-alternatives --config x-terminal-emulator
 # xfce4-terminal &
+xfce4-terminal --maximize &
 /usr/sbin/sshd -D
--- myUbuntu/runPutty.bat
+++ myUbuntu/runPutty.bat
@@ -1,2 +1,5 @@
 @REM note that putty uses "-P" uppercase for port
-putty root@localhost -P 22222 -i id.ppk
+@REM putty root@localhost -P 22222 -i id.ppk
+@REM I could figure out how to set UTF-8 by putty command line
+@REM Settings in putty are Window->Translation.  Oh well ... use 'load'
+putty -load my-ubuntu  -P 22222 -i id.ppk
